1754bddfa740407a166bf60c87f669454e806309
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-08-22 13:03:13 -0600

    tricks schema created

--- migrations/20200821224416_tricks_table.js
+++ migrations/20200821224416_tricks_table.js
@@ -1,6 +1,45 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('tricks', function(table) {
+        table
+            .increments()
+            .unsigned();
+        table
+            .string('user_id')
+            .notNullable();
+        table
+            .string('title')
+            .notNullable();
+        table
+            .string('summary')
+            .notNullable();
+        table
+            .string('body')
+            .notNullable();
+        table
+            .integer('likes')
+            .notNullable()
+            .unsigned()
+            .defaultTo(0);
+        table
+            .integer('dislikes')
+            .notNullable()
+            .unsigned()
+            .defaultTo(0);
+        table
+            .integer('tried')
+            .notNullable()
+            .unsigned()
+            .defaultTo(0);
+        table
+            .integer('views')
+            .notNullable()
+            .unsigned()
+            .defaultTo(0);
+        table.specificType('tags', 'text ARRAY');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
 };
 
 exports.down = function(knex) {
